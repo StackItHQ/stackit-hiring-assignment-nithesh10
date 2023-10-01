4c41a9b912e401697253b280cce883abb914aac1
nithesh10 <a.nitheshkumar@gmail.com>
2023-10-01 16:12:09 +0530

    filter column names and display

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, redirect, url_for, flash
+import pandas as pd
 from werkzeug.utils import secure_filename
 import os
 
@@ -31,12 +32,22 @@ def upload_file():
         filename_without_extension=str(os.path.splitext(filename)[0])
         print("Filename is ",filename_without_extension)
         gsheets.create_new_google_sheet(filename_without_extension)
-        selected_columns = ["Column1", "Column2"]
+        return redirect(url_for('display_csv', filename_without_extension=filename_without_extension))
+
+
+@app.route('/display_csv/<filename_without_extension>', methods=['GET', 'POST'])
+def display_csv(filename_without_extension):
+    filepath=os.path.join(app.config['UPLOAD_FOLDER'], (filename_without_extension+".csv"))
+    if request.method == 'POST':
+        selected_columns = request.form.getlist('selected_columns')
+        # Pass the selected columns to the import_csv function
         result = gsheets.import_csv(filename_without_extension, filepath, selected_columns)
         print(result)
-        
-
         return redirect(url_for('index'))
+    df = pd.read_csv(filepath)
+    columns = df.columns.tolist()
+    # Render the template to display the DataFrame
+    return render_template('display_csv.html', df=df, columns=columns,filename=filename_without_extension)
 
 if __name__ == '__main__':
     app.run()
