816ab8e07c084c482bf2ed9ea1a437ba6d570c12
nithesh10 <a.nitheshkumar@gmail.com>
2023-10-01 13:46:08 +0530

    basic csv importer modules done

--- app/gsheets.py
+++ app/gsheets.py
@@ -1,6 +1,9 @@
 # gsheets.py
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
+import io
+from googleapiclient.http import MediaIoBaseUpload
 from app import app
 
 class gsheets_api:
@@ -11,38 +14,56 @@ class gsheets_api:
         # Authenticate with Google Sheets API using credentials
         scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
         credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            app.config['GOOGLE_SHEETS_CREDS'], scope)
+            app.config['SHEETS_JSON'], scope)
         self.gc = gspread.authorize(credentials)
+        self.drive_service = build('drive', 'v3', credentials=credentials)
+    
+    def create_new_google_sheet(self, sheet_title):
+        self.spreadsheet=self.gc.create(sheet_title)
+        worksheet = self.spreadsheet.get_worksheet(0)
 
     def open_spreadsheet(self,title):
-        spreadsheet = self.gc.open(title)
-        return spreadsheet
+        self.spreadsheet = self.gc.open(title)
+        return self.spreadsheet
     
-    def create_new_sheet(self,sheet_title):
-        # Create a new worksheet with the specified title
-        self.gc.add_worksheet(title=sheet_title, rows="100", cols="20")
+    def generate_shareable_link(self):
+        
+        self.spreadsheet.share('', perm_type='anyone', role='reader')
+        spreadsheet_link = self.spreadsheet.url
+        return spreadsheet_link
+    
+    def create_new_worksheet(self,sheet_title):
+        self.spreadsheet.add_worksheet(title=sheet_title, rows="100", cols="20")
         return f"Created a new sheet with the title: {sheet_title}"
     
-    def import_csv(self, spreadsheet_title, file_path, column_mapping):
-        spreadsheet = self.open_spreadsheet(spreadsheet_title)
-        worksheet = spreadsheet.get_worksheet(0)  # Change the index as needed
+    def import_csv(self, spreadsheet_title, file_path, selected_columns):
+        # Get the first worksheet (you can adjust the index if needed)
+        worksheet = self.spreadsheet.get_worksheet(0)
 
         # Read and process the CSV file
         with open(file_path, 'r') as file:
             lines = file.readlines()
 
         headers = lines[0].strip().split(',')
-        mapped_headers = [column_mapping.get(header, header) for header in headers]
-
+        
+        # Create a column mapping dictionary for selected columns
+        column_mapping = {header: header for header in headers if header in selected_columns}
+        print(column_mapping)
         # Write the mapped headers to the Google Sheet
-        worksheet.insert_rows(mapped_headers, 1)
+        worksheet.update('A1', [list(column_mapping.values())])
 
-        # Insert the data rows
+        # Insert the data rows for selected columns
+        data_rows = []
         for line in lines[1:]:
             data = line.strip().split(',')
-            worksheet.append_table(data)
+            selected_data = [data[headers.index(col)] for col in column_mapping.keys()]
+            data_rows.append(selected_data)
+        
+        # Append the data to the worksheet
+        worksheet.append_rows(data_rows)
 
-        return f"CSV data imported into '{spreadsheet_title}'"
+        return self.generate_shareable_link()
+
 
     def select_columns(self, spreadsheet_title, selected_columns):
         spreadsheet = self.open_spreadsheet(spreadsheet_title)
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,10 @@ from werkzeug.utils import secure_filename
 import os
 
 from app import app
+from app.gsheets import gsheets_api  
+
+# Create an instance of the gsheets_api class
+gsheets = gsheets_api()
 
 @app.route('/')
 @app.route('/index')
@@ -20,8 +24,18 @@ def upload_file():
         return redirect(request.url)
     if file:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        filepath=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
         flash('File uploaded successfully')
+        
+        filename_without_extension=str(os.path.splitext(filename)[0])
+        print("Filename is ",filename_without_extension)
+        gsheets.create_new_google_sheet(filename_without_extension)
+        selected_columns = ["Column1", "Column2"]
+        result = gsheets.import_csv(filename_without_extension, filepath, selected_columns)
+        print(result)
+        
+
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
